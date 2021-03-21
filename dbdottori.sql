2688271f182c1b56ab54ae31a02154b1a27c4c4d
naxy2 <mr.dieonni@gmail.com>
2021-03-21 16:46:12 +0100

    aggiunti valori database

--- dbdottori.sql
+++ dbdottori.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Mar 19, 2021 alle 18:58
--- Versione del server: 10.4.16-MariaDB
--- Versione PHP: 7.4.12
+-- Creato il: Mar 21, 2021 alle 16:45
+-- Versione del server: 10.4.17-MariaDB
+-- Versione PHP: 8.0.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -35,6 +35,14 @@ CREATE TABLE `associazione` (
   `fkPaziente` varchar(16) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dump dei dati per la tabella `associazione`
+--
+
+INSERT INTO `associazione` (`data`, `fkMedico`, `fkPaziente`) VALUES
+('0000-00-00', 'ABC12', 'AAAAAA00A00A000A'),
+('0000-00-00', 'ABC12', 'AAAAAA00A11A000A');
+
 -- --------------------------------------------------------
 
 --
@@ -48,6 +56,14 @@ CREATE TABLE `medico` (
   `dataNascita` date NOT NULL,
   `luogoNascita` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dump dei dati per la tabella `medico`
+--
+
+INSERT INTO `medico` (`codice`, `cognome`, `nome`, `dataNascita`, `luogoNascita`) VALUES
+('ABC11', 'Palmieri', 'Romeo', '1965-10-11', 'Palermo'),
+('ABC12', 'Esposito', 'Ciro', '1969-12-12', 'Napoli');
 
 -- --------------------------------------------------------
 
@@ -65,6 +81,16 @@ CREATE TABLE `paziente` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Dump dei dati per la tabella `paziente`
+--
+
+INSERT INTO `paziente` (`CF`, `cognome`, `nome`, `dataNascita`, `luogoNascita`, `indirizzo`) VALUES
+('AAAAAA00A00A000A', 'Costa', 'Alessandro', '2000-10-10', 'CIVITAVECCHIA', 'casa sua'),
+('AAAAAA00A00A000B', 'Rossi', 'Mario', '1990-02-21', 'Roma', 'via Lazio 15'),
+('AAAAAA00A00A000C', 'Rossi', 'Alessandro', '2000-12-12', 'Roma', 'casa sua'),
+('AAAAAA00A11A000A', 'Oniarti ', 'Diego', '2002-10-18', 'Trento', 'casa mia');
+
+--
 -- Indici per le tabelle scaricate
 --
 
@@ -72,8 +98,9 @@ CREATE TABLE `paziente` (
 -- Indici per le tabelle `associazione`
 --
 ALTER TABLE `associazione`
-  ADD PRIMARY KEY (`fkMedico`,`fkPaziente`),
-  ADD KEY `associazione_paziente` (`fkPaziente`);
+  ADD PRIMARY KEY (`fkPaziente`),
+  ADD KEY `fkPaziente` (`fkPaziente`),
+  ADD KEY `fkMedico` (`fkMedico`);
 
 --
 -- Indici per le tabelle `medico`
@@ -95,8 +122,8 @@ ALTER TABLE `paziente`
 -- Limiti per la tabella `associazione`
 --
 ALTER TABLE `associazione`
-  ADD CONSTRAINT `associazione_medico` FOREIGN KEY (`fkMedico`) REFERENCES `medico` (`codice`),
-  ADD CONSTRAINT `associazione_paziente` FOREIGN KEY (`fkPaziente`) REFERENCES `paziente` (`CF`);
+  ADD CONSTRAINT `associazione_paziente` FOREIGN KEY (`fkPaziente`) REFERENCES `paziente` (`CF`),
+  ADD CONSTRAINT `fkMedico` FOREIGN KEY (`fkMedico`) REFERENCES `medico` (`codice`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
