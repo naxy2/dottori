c7af1b3a915c3dcfbb67a7018eaf3b2dfd114686
naxy2 <mr.dieonni@gmail.com>
2021-03-23 19:51:08 +0100

    fix

--- sito/SCRIPT/aggiungiAssociazione.php
+++ sito/SCRIPT/aggiungiAssociazione.php
@@ -8,7 +8,7 @@ if (isset($_POST['medico']) && isset($_POST['paziente']) && isset($_POST['data']
     include('./connetti.php');
     session_start();
 
-    mysqli_query($connessione, "DELETE FROM `associazione` WHERE `fkPaziente`=$paziente");
+    //mysqli_query($connessione, "DELETE FROM `associazione` WHERE `fkPaziente`=$paziente");
     if ($data==""){
         $successo = mysqli_query($connessione, "INSERT INTO `associazione`(`fkMedico`, `fkPaziente`) VALUES ('$medico','$paziente')");
     }else{
