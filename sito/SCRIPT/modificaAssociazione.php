dd6db48a6bc0d4b1dd45fffa6c3f14f6fa2a561d
diego <mr.dieonni@gmail.com>
2021-04-10 12:59:42 +0200

    fixato script modificaAssociazione.php

--- sito/SCRIPT/modificaAssociazione.php
+++ sito/SCRIPT/modificaAssociazione.php
@@ -2,7 +2,7 @@
 if (isset($_POST['paziente']) && isset($_POST['dottore'])){
     include("./connetti.php");
     //echo "UPDATE `associazione` SET `data`=NULL,`fkMedico`='{$_GET['dottore']}' WHERE fkPaziente='{$_GET['paziente']}'";
-    mysqli_query($connessione, "UPDATE `associazione` SET `data`=default,`fkMedico`='{$_POST['dottore']}' WHERE fkPaziente='{$_POST['paziente']}'");
+    mysqli_query($connessione, "UPDATE `paziente` SET `dataAssociazione`=CURRENT_TIMESTAMP(),`fkMedico`='{$_POST['dottore']}' WHERE CF='{$_POST['paziente']}'");
     header("location:../modificaAssociazione.php?paziente={$_POST['paziente']}");
 }else{
     header("location:../modificaAssociazione.php");
